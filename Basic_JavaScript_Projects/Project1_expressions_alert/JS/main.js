e1e351c1166717efb48b68c4295652eff6ee26a2
Dusty Zoland <DikaiosuneZ@gmail.com>
2020-10-29 12:15:59 -0700

    Complete first Tech Academy Assignments

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+
+//First thing when the user loads in is a friendly greeting.
+var alertMessage = "Hello, World!";
+window.alert(alertMessage);
+
+//Doing some basic math
+var expression = 22*3;
+
+//Practicing creating a string with quotation marks
+//then creating another variable within that declaration
+var A ="He said, \"Do you know how to include quotation marks in a string?\"", B=" I told him load the page."
+
+//Changing the font color of both variables
+A = A.fontcolor("red");
+B = B.fontcolor("blue");
+
+//Concatenating the strings when calling them
+document.write(A+B);
+
+//Making sure the math for var expression worked.
+document.write(expression);
